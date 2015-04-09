0343bf9fada91045b42d1acda4075e8a801596ea
RemiZOffAlex <remizoffalex@gmail.com>
2015-04-09 20:51:56 +0300

    SugarCRM для CentOS 6

--- scripts/cp/sugarcrm/install.sh
+++ scripts/cp/sugarcrm/install.sh
@@ -30,6 +30,7 @@ pid-file=/var/run/mysqld/mysqld.pid
 EOF
 
 # Загруза и распаковка SugarCRMmkdir -p /data/www
+mkdir -p /data/www
 wget http://garr.dl.sourceforge.net/project/sugarcrm/1%20-%20SugarCRM%206.5.X/SugarCommunityEdition-6.5.X/SugarCE-6.5.20.zip
 unzip SugarCE-6.5.20.zip
 mv SugarCE-Full-6.5.20 /data/www/sugarcrm
@@ -52,12 +53,32 @@ cat <<EOF > /etc/httpd/vhosts/sugarcrm.conf
 </VirtualHost>
 EOF
 
-echo "NameServer 80" >> /etc/httpd/conf/httpd.conf
+echo "NameVirtualHost *:80" >> /etc/httpd/conf/httpd.conf
 echo "include vhosts/*.conf" >> /etc/httpd/conf/httpd.conf
+chkconfig httpd on
+service httpd start
 
 # crontab
-*    *    *    *    *     cd /data/www/sugarcrm; php -f cron.php > /dev/null 2>&1 
+echo "*    *    *    *    *     cd /data/www/sugarcrm; php -f cron.php > /dev/null 2>&1" >> /etc/crontab
 
 # php.ini
 cp /etc/php.ini /etc/php.ini.orig
 sed 's/upload_max_filesize =.*/upload_max_filesize = 16M/' /etc/php.ini.orig > /etc/php.ini
+
+# iptables
+cat <<EOF > /etc/sysconfig/iptables
+# Firewall configuration written by system-config-firewall
+# Manual customization of this file is not recommended.
+*filter
+:INPUT ACCEPT [0:0]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+-A INPUT -p icmp -j ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+-A INPUT -j REJECT --reject-with icmp-host-prohibited
+-A FORWARD -j REJECT --reject-with icmp-host-prohibited
+COMMIT
+EOF
