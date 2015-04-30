2d12ea3f31bf9b86b23aa5db2b92ec9a5c20d5bc
servancho <bxtgroup@gmail.com>
2015-05-01 00:07:34 +0300

    Updated nginx vhosts builder

--- scripts/directadmin/nginx/nginxvhostctl.py
+++ scripts/directadmin/nginx/nginxvhostctl.py
@@ -1,3 +1,50 @@
+# Copyright (C) 2015 JustHost.ru, Dmitry Shilyaev
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+# Script is used for DirectAdmin and Nginx integration. It generates map files
+# for Nginx to parametrize virtual hosts. It supports SSL. You must include
+# OUT_CONFIG_DIR in nginx.conf file to activate maps and SSL virtual hosts.
+#
+# usage: nginxvhostctl.py [-h] -o OUT_CONFIG_DIR [-s DA_USERS_CONFIG_DIR]
+# [--rebuild-all | -r REBUILD_USER | -d DELETE_USER]
+#
+# Nginx virtualhosts updater
+#
+# optional arguments:
+#   -h, --help            show this help message and exit
+#   -o OUT_CONFIG_DIR, --out-dir OUT_CONFIG_DIR
+#                         Directory with generated Nginx config files (default:
+#                         None)
+#   -s DA_USERS_CONFIG_DIR, --da-users-config DA_USERS_CONFIG_DIR
+#                         DirectAdmin users config root (default:
+#                         /usr/local/directadmin/data/users)
+#   --rebuild-all         Rebuild vhosts using DirectAdmin user configs for all
+#                         users (default: False)
+#   -r REBUILD_USER, --rebuild REBUILD_USER
+#                         Rebuild vhosts using DirectAdmin user configs for the
+#                         specific user (default: None)
+#   -d DELETE_USER, --delete DELETE_USER
+#                         Rebuild vhosts using DirectAdmin user configs for the
+#                         specific user (default: None)
+#
 import argparse
 import exceptions
 import errno
