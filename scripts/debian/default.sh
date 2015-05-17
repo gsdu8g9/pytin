6eef07253af214a28e52ee79f3fa232e2492498a
RemiZOffAlex <remizoffalex@gmail.com>
2015-05-17 20:38:16 +0300

    Ключи Ubuntu 14.04

--- scripts/debian/default.sh
+++ scripts/debian/default.sh
@@ -9,6 +9,18 @@
 # Requirements:
 #	Debian/Ubuntu
 
+# Detect OS
+case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
+Debian)     typeos="debian" ;;
+Ubuntu)
+    case $(head -n1 /etc/issue | grep -o '14.04') in
+    14.04)
+        sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 40976EAF437D05B5
+        sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3B4FE6ACC0B21F32
+    esac
+    *)          typeos="rhel" ;;
+esac
+
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install wget nano ntpdate ntp
