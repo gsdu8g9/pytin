3a9c389909b42d4594fe6a14ebea16e291d81d34
RemiZOffAlex <remizoffalex@gmail.com>
2015-07-06 17:15:25 +0300

    Vesta

--- scripts/vesta/setup.sh
+++ scripts/vesta/setup.sh
@@ -13,6 +13,7 @@
 #   CentOS
 #       bash <(curl https://raw.githubusercontent.com/servancho/pytin/master/scripts/vesta/setup.sh)
 
+function set_conf {
 # APF
 perl -pi -e 's/IG_TCP_CPORTS="[^\"]*"/IG_TCP_CPORTS="21,22,25,80,110,143,443,465,587,953,993,995,2525,8080,8083,8443"/g' /etc/apf/conf.apf
 perl -pi -e 's/IG_UDP_CPORTS="[^\"]*"/IG_UDP_CPORTS="53,953"/g' /etc/apf/conf.apf
@@ -20,6 +21,7 @@ perl -pi -e 's/IG_UDP_CPORTS="[^\"]*"/IG_UDP_CPORTS="53,953"/g' /etc/apf/conf.ap
 # Проблемы с лимитом на открытые файлы
 echo "*               soft    nofile          8192" >> /etc/security/limits.conf 
 echo "*               hard    nofile          8192" >> /etc/security/limits.conf 
+}
 
 # Detect OS
 case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
@@ -27,6 +29,7 @@ Debian)
 
 wget --no-check-certificate https://raw.githubusercontent.com/servancho/pytin/master/scripts/debian/default.sh
 bash default.sh
+set_conf
 wget http://vestacp.com/pub/vst-install.sh
 bash vst-install.sh
 
@@ -35,6 +38,7 @@ Ubuntu)
 
 wget --no-check-certificate https://raw.githubusercontent.com/servancho/pytin/master/scripts/debian/default.sh
 bash default.sh
+set_conf
 wget http://vestacp.com/pub/vst-install.sh
 bash vst-install.sh
 
@@ -43,6 +47,7 @@ bash vst-install.sh
 CentOS)
 
 bash <(curl https://raw.githubusercontent.com/servancho/pytin/master/scripts/centos/default.sh)
+set_conf
 bash <(curl http://vestacp.com/pub/vst-install.sh)
 
 ;;
