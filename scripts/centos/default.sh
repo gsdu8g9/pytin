973e4226726309827527ad2f2e91d13d8a3f49f1
RemiZOffAlex <remizoffalex@gmail.com>
2015-07-13 15:13:11 +0300

    Настройка apf в случае с venet

--- scripts/centos/default.sh
+++ scripts/centos/default.sh
@@ -43,8 +43,9 @@ perl -pi -e 's/DEVEL_MODE="1"/DEVEL_MODE="0"/g' /etc/apf/conf.apf
 ip link | grep venet
 if [ $? -eq 0 ];
 then
-    perl -pi -e 's/IFACE_IN="eth0"/IFACE_IN="venet0:0"/g' /etc/apf/conf.apf
-    perl -pi -e 's/IFACE_OUT="eth0"/IFACE_OUT="venet0:0"/g' /etc/apf/conf.apf
+    perl -pi -e 's/IFACE_IN="eth0"/IFACE_IN="venet0"/g' /etc/apf/conf.apf
+    perl -pi -e 's/IFACE_OUT="eth0"/IFACE_OUT="venet0"/g' /etc/apf/conf.apf
+    perl -pi -e 's/SET_MONOKERN="0"/SET_MONOKERN="1"/g' /etc/apf/conf.apf
 fi
 
 echo "10.0.0.0/24" >> /etc/apf/allow_hosts.rules
