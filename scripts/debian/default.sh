6a9cd814aee925552869c5eff89d99e0439a3498
RemiZOffAlex <remizoffalex@gmail.com>
2015-07-15 14:56:33 +0300

    Debian

--- scripts/debian/default.sh
+++ scripts/debian/default.sh
@@ -46,3 +46,33 @@ ntpdate -d ntp1.vniiftri.ru
 
 sed -i 's/.*history-search-backward$/"\\e[A": history-search-backward/g' /etc/inputrc
 sed -i 's/.*history-search-forward$/"\\e[B": history-search-forward/g' /etc/inputrc
+
+### Install BFD and APF
+mkdir secdistr && cd secdistr
+
+mkdir apf && cd apf
+wget http://www.rfxn.com/downloads/apf-current.tar.gz
+tar --strip-components=1 -xzf apf-current.tar.gz
+./install.sh
+cd ..
+
+cp /etc/apf/conf.apf /etc/apf/conf.apf.bkp
+perl -pi -e 's/DEVEL_MODE="1"/DEVEL_MODE="0"/g' /etc/apf/conf.apf
+
+# For OpenVZ
+ip link | grep venet
+if [ $? -eq 0 ];
+then
+    perl -pi -e 's/IFACE_IN="eth0"/IFACE_IN="venet0"/g' /etc/apf/conf.apf
+    perl -pi -e 's/IFACE_OUT="eth0"/IFACE_OUT="venet0"/g' /etc/apf/conf.apf
+    perl -pi -e 's/SET_MONOKERN="0"/SET_MONOKERN="1"/g' /etc/apf/conf.apf
+fi
+
+echo "10.0.0.0/24" >> /etc/apf/allow_hosts.rules
+
+
+mkdir bfd && cd bfd
+wget http://www.rfxn.com/downloads/bfd-current.tar.gz
+tar --strip-components=1 -xzf bfd-current.tar.gz
+./install.sh
+cd ..
