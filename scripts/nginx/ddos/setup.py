8dcf86edfe3b62e1c75b5aaa1e9637fcc2a1cb20
RemiZOffAlex <remizoffalex@gmail.com>
2015-09-21 00:30:06 +0300

    Инсталлятор ddos

--- scripts/nginx/ddos/setup.py
+++ scripts/nginx/ddos/setup.py
@@ -4,6 +4,7 @@ from os.path import join, dirname
 setup(
     name='ddos',
     version='0.0.1',
-    packages=find_packages(exclude=["directadmin.*", "directadmin"]),
+    packages=find_packages(exclude=["tests.*", "ddos.egg-info"]),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    scripts = ['http_protector.py'],
 )
