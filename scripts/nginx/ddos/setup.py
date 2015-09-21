13364f7a85df8723fad71923a23c6be297a074c8
RemiZOffAlex <remizoffalex@gmail.com>
2015-09-21 17:24:21 +0300

    Debian FAI

--- scripts/nginx/ddos/setup.py
+++ scripts/nginx/ddos/setup.py
@@ -5,6 +5,7 @@ setup(
     name='ddos',
     version='0.0.1',
     packages=find_packages(exclude=["tests.*", "ddos.egg-info"]),
+    url='https://justhost.ru/'
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
     scripts = ['http_protector.py'],
 )
