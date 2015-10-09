27a40693a49335c4a94ffca3e562699a62208bb0
RemiZOffAlex <remizoffalex@gmail.com>
2015-10-10 01:23:46 +0300

    Установщик YouTrack

--- scripts/youtrack/setup.sh
+++ scripts/youtrack/setup.sh
@@ -35,9 +35,16 @@ chown youtrack /var/run/youtrack
 
 # Download and install
 wget http://download-cf.jetbrains.com/charisma/youtrack-6.5.16807.jar -O /home/youtrack/youtrack.jar
-yum -y install java-1.8.0-openjdk
+yum -y install java-1.8.0-openjdk rng-tools
 
 wget https://raw.githubusercontent.com/servancho/pytin/master/scripts/youtrack/youtrack -O /etc/init.d/youtracker
+wget https://raw.githubusercontent.com/servancho/pytin/master/scripts/youtrack/youtrack.sh -O /etc/init.d/youtracker.sh
+
+cat <<EOF > /etc/ld.so.conf.d/java-libjli.conf
+/usr/lib/jvm/jre-1.8.0-openjdk.x86_64/lib/amd64/jli
+EOF
+
+setcap 'cap_net_bind_service=+epi' /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
 
 chmod +x /etc/init.d/youtracker
 
