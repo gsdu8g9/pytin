1deb5ed3e00836e82f11309562fc6490abe15085
servancho <bxtgroup@gmail.com>
2016-03-26 15:32:24 +0300

    Fixed 3com 2250 SNMP walker

--- cmdb/importer/providers/vendors/sw3com.py
+++ cmdb/importer/providers/vendors/sw3com.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals
 
-from types import ObjectType
-
-from pysnmp.smi.rfc1902 import ObjectIdentity
+from pysnmp.smi.rfc1902 import ObjectIdentity, ObjectType
 
 from cmdb.settings import logger
 from importer.providers.l3_switch import L3Switch, L3SwitchPort, _snmp_walk
@@ -26,7 +24,7 @@ class Switch3Com2250(L3Switch):
         assert community
 
         # load port id map
-        oid = ObjectType(ObjectIdentity('.1.3.6.1.2.1.17.1.4.1.2'))
+        oid = ObjectType(ObjectIdentity('1.3.6.1.2.1.17.1.4.1.2'))
         for name, value in _snmp_walk(host, community, oid):
             self.snmpid__port_num__map[value] = name.split('.')[-1]
 
@@ -53,6 +51,5 @@ class Switch3Com2250(L3Switch):
         # arp address table
         oid = ObjectType(ObjectIdentity('IP-MIB', 'ipNetToMediaPhysAddress'))
         for name, value in _snmp_walk(host, community, oid):
-            name_parts = name.split('.')
-            ip_address = ".".join([name_parts[x] for x in range(len(name_parts) - 4, len(name_parts))])
-            self._add_server_port_ip(value[2:].upper(), ip_address)
+            ip_address = '.'.join(name.split('.')[-4:])
+            self._add_server_port_ip(value.upper(), ip_address)
