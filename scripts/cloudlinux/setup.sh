8f68f836865e71d8c25381af96d69354d43c855a
servancho <bxtgroup@gmail.com>
2016-04-10 22:18:08 +0300

    Cloud fix

--- scripts/cloudlinux/setup.sh
+++ scripts/cloudlinux/setup.sh
@@ -42,25 +42,16 @@ if [ ! -e ./firstrun ]; then
 
     wget http://repo.cloudlinux.com/cloudlinux/sources/cln/cldeploy
     sh cldeploy -k ${activation_key}
-    yum -y install lvemanager
-    yum -y install cagefs
     echo "[!!!] Don't forget to reboot and run this script again."
 
     echo "1" > ./firstrun
 else
+    yum -y install lvemanager cagefs
+
+    cagefsctl --init
+
     yum -y groupinstall alt-php
-    yum -y reinstall cagefs lvemanager
-    /usr/sbin/cagefsctl --init
 
-
-    # CloudLinux doesn't provide support for ffmpeg related libraries due to legal/patent
-    # restrictions associated with that product. If you believe that you are not subject to
-    # such legal restrictions, or have a license to use ffmpeg licenses from the patent right
-    # holders, you can install needed libraries by installing EPEL and RPM Fusion repositories.
-    rpm -ivh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-    rpm -ivh http://download1.rpmfusion.org/free/el/updates/6/x86_64/rpmfusion-free-release-6-1.noarch.rpm
-
-    yum -y install ffmpeg ffmpeg-libs lame-libs librtmp x264-libs xvidcore
     cagefsctl --force-update
 
     echo "[!] Don't forget to reboot."
